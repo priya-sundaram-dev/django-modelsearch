f959cfd82e64ebd507eaa28398444e815a72410c
Matthew Westcott <matthew@west.co.tt>
2026-09-10 14:46:49 +0100

    Account for catch_indexing_errors flag in treebeard signals

--- modelsearch/signal_handlers.py
+++ modelsearch/signal_handlers.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db.models.signals import post_delete, post_save
 
 from . import index
@@ -21,6 +23,9 @@ except ImportError:
     NS_Node = None
 
 
+logger = logging.getLogger("modelsearch.index")
+
+
 def post_save_signal_handler(instance, **kwargs):
     if kwargs.get("raw", False):
         return
@@ -36,53 +41,88 @@ def post_delete_signal_handler(instance, **kwargs):
 
 def indexes_with_method(model, method_name):
     """
-    Returns an iterator of (backend_name, index) tuples for all auto-updating indexes for the given
-    model that have the given method
+    Returns an iterator of (backend_name, backend, index) tuples for all auto-updating indexes for
+    the given model that have the given method
     """
     for backend_name, backend in index.get_search_backends_with_name(
         with_auto_update=True
     ):
         index_obj = backend.get_index_for_model(model)
         if hasattr(index_obj, method_name):
-            yield (backend_name, index_obj)
+            yield (backend_name, backend, index_obj)
+
+
+# Signal handlers for treebeard signals emitted during tree move operations. These are handled
+# in-process rather than offloaded to a task queue, because they often involve multiple signals
+# that need to be processed in order.
 
 
 def mp_tree_path_updated_signal_handler(sender, old_path, new_path, **kwargs):
-    for _backend_name, index_obj in indexes_with_method(
+    for _backend_name, backend, index_obj in indexes_with_method(
         sender, "process_mptree_path_updated"
     ):
-        index_obj.process_mptree_path_updated(sender, old_path, new_path)
+        try:
+            index_obj.process_mptree_path_updated(sender, old_path, new_path)
+        except Exception as e:
+            logger.exception(
+                "Error processing mptree.path_updated signal for model %s: %s",
+                sender.__name__,
+                e,
+            )
+            if not backend.catch_indexing_errors:
+                raise
 
 
 def ns_tree_gap_altered_signal_handler(sender, tree_id, start_index, offset, **kwargs):
-    # Do not enqueue this as an asynchronous task, because an NS_Node move operation involves multiple signals
-    # that need to be processed in order
-    for _backend_name, index_obj in indexes_with_method(
+    for _backend_name, backend, index_obj in indexes_with_method(
         sender, "process_nstree_gap_altered"
     ):
-        index_obj.process_nstree_gap_altered(sender, tree_id, start_index, offset)
+        try:
+            index_obj.process_nstree_gap_altered(sender, tree_id, start_index, offset)
+        except Exception as e:
+            logger.exception(
+                "Error processing nstree.gap_altered signal for model %s: %s",
+                sender.__name__,
+                e,
+            )
+            if not backend.catch_indexing_errors:
+                raise
 
 
 def ns_tree_subtree_moved_signal_handler(
     sender, tree_id, lft, rgt, target_tree_id, index_offset, depth_offset, **kwargs
 ):
-    # Do not enqueue this as an asynchronous task, because an NS_Node move operation involves multiple signals
-    # that need to be processed in order
-    for _backend_name, index_obj in indexes_with_method(
+    for _backend_name, backend, index_obj in indexes_with_method(
         sender, "process_nstree_subtree_moved"
     ):
-        index_obj.process_nstree_subtree_moved(
-            sender, tree_id, lft, rgt, target_tree_id, index_offset, depth_offset
-        )
+        try:
+            index_obj.process_nstree_subtree_moved(
+                sender, tree_id, lft, rgt, target_tree_id, index_offset, depth_offset
+            )
+        except Exception as e:
+            logger.exception(
+                "Error processing nstree.subtree_moved signal for model %s: %s",
+                sender.__name__,
+                e,
+            )
+            if not backend.catch_indexing_errors:
+                raise
 
 
 def ns_tree_tree_ids_incremented_signal_handler(sender, min_tree_id, **kwargs):
-    # Do not enqueue this as an asynchronous task, because an NS_Node move operation involves multiple signals
-    # that need to be processed in order
-    for _backend_name, index_obj in indexes_with_method(
+    for _backend_name, backend, index_obj in indexes_with_method(
         sender, "process_nstree_tree_ids_incremented"
     ):
-        index_obj.process_nstree_tree_ids_incremented(sender, min_tree_id)
+        try:
+            index_obj.process_nstree_tree_ids_incremented(sender, min_tree_id)
+        except Exception as e:
+            logger.exception(
+                "Error processing nstree.tree_ids_incremented signal for model %s: %s",
+                sender.__name__,
+                e,
+            )
+            if not backend.catch_indexing_errors:
+                raise
 
 
 def register_signal_handlers():
